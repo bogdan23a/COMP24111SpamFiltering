b28251fcd84ffc797399b60ea83cdd71a58e3b21
bogdan23a <gbogdan23a@yahoo.com>
2017-11-20 05:44:06 +0200

    Working for n att and 3 class

--- NBTrain.m
+++ NBTrain.m
@@ -1,21 +1,29 @@
 %
 %Setup variables
 %
-maxAttributeNumber = 2;
-
+maxAttributeNumber = 6;
+classNumber = 3;
 
 
 
 Yes=0;
-for i = 1:2300
-	Yes = Yes + LabelSet(i);
+No=0;
+Maybe=0;
+for i = 1:2301
+	if LabelSet(i) == 2
+		Yes = Yes + 1;
+	elseif LabelSet(i) == 1
+		Maybe = Maybe + 1;
+	else
+		No = No + 1;
+	end
 end
-No = 2300 - Yes;
 
 %
 %P(Yes),P(No)
 %
 YesProbability = Yes / 2300;
+MaybeProbability = Maybe / 2300;
 NoProbability = No / 2300;
 
 %
@@ -26,20 +34,24 @@ NoProbability = No / 2300;
 
 %numOfClasses = 2;
 
-Parameters = zeros(57,(maxAttributeNumber + 1)*2);
+Parameters = zeros(57,(maxAttributeNumber + 1)*classNumber);
 
 for i = 1:57
   currentAttributeNumber = maxAttributeNumber;
   while currentAttributeNumber >= 0
-	  for j = 1:2300
-	  	if LabelSet(j) == 1
+	  for j = 1:2301
+	  	if LabelSet(j) == 2
 	  	  
 	  	  if(AttributeSet(j,i) == currentAttributeNumber)
 	  	  	Parameters(i, maxAttributeNumber - currentAttributeNumber + 1) = Parameters(i, maxAttributeNumber - currentAttributeNumber + 1) + 1;
 	  	  end
-	   	else
+	   	elseif LabelSet(j) == 1
 	   	  if(AttributeSet(j,i) == currentAttributeNumber)
 	  	  	Parameters(i, 2 * maxAttributeNumber - currentAttributeNumber + 2) = Parameters(i, 2 * maxAttributeNumber - currentAttributeNumber + 2) + 1;
+	  	  end
+	  	  else
+	  	  	if(AttributeSet(j,i) == currentAttributeNumber)
+	  	  	Parameters(i, 3 * maxAttributeNumber - currentAttributeNumber + 3) = Parameters(i, 3 * maxAttributeNumber - currentAttributeNumber + 3) + 1;
 	  	  end
 		end
 	  end
@@ -49,21 +61,23 @@ for i = 1:57
 end
 
 
-LookUpTable = zeros(57,(maxAttributeNumber + 1)*2);
+LookUpTable = zeros(57,(maxAttributeNumber + 1)*classNumber);
 for i=1:57
-	%for j=1:maxAttributeNumber+1
-	%	LookUpTable(i,j) = Parameters(i,j)/Yes;
-	%end
-	%for j=maxAttributeNumber+2,(maxAttributeNumber + 1)*2
-		%LookUpTable(i,j) = Parameters(i,j)/No;
-	%	LookUpTable(i,j) = 123;
-	%end
-	for j=1:(maxAttributeNumber + 1)*2
+	for j=1:(maxAttributeNumber + 1)*classNumber
 		if j<maxAttributeNumber + 2 
 			LookUpTable(i,j) = Parameters(i,j)/Yes;
+		elseif j>=maxAttributeNumber+1 & j<=(maxAttributeNumber+1)*2 
+			LookUpTable(i,j) = Parameters(i,j)/Maybe;
 		else
 			LookUpTable(i,j) = Parameters(i,j)/No;
 		end
 	end
 end
-
+%
+%Check if adds to 1
+%
+s=0;
+for i=1:21
+	s=s+LookUpTable(1,i);
+end
+s
